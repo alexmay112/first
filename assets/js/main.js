ee8e7d9c6691cb560f3c1a1d882323a3e81acd63
O.M <a.majstrenko@everad.com>
2019-08-01 15:30:21 +0300

    fix rotate for review-list button

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,6 +47,7 @@ $(document).ready(function () {
 
   $('.review-list__link').on('click', function (e) {
     e.preventDefault();
-    $(this).siblings('.review-list__item-discription').toggleClass('review-list__item-discription_expand')
+    $(this).siblings('.review-list__item-discription').toggleClass('review-list__item-discription_expand');
+    $(this).children('.review-list__link-img').toggleClass('review-list__link-img_rotate')
   })
 });
